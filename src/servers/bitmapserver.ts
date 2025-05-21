8eb3586736537037954d4e59472aee36092b0cf6
Thomas Ball <thomasjball@users.noreply.github.com>
2025-05-21 17:46:31 +0000

    add bitmapserver.ts

---
+++ src/servers/bitmapserver.ts
@@ -0,0 +1,13 @@
+import { BuzzerCmd, BuzzerReg, SRV_BUZZER } from "../jdom/constants"
+import { jdpack, jdunpack } from "../jdom/pack"
+import { Packet } from "../jdom/packet"
+import { JDRegisterServer } from "../jdom/servers/registerserver"
+import { JDServiceServer, JDServerOptions } from "../jdom/servers/serviceserver"
+
+/**
+ * Server implementation for the bitmap service
+ * @category Servers
+ */
+export class BitmapServer extends JDServiceServer {
+
+}
