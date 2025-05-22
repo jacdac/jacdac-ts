69e90c81fbde299aa7c2bdde745183f6b8953c63
Thomas Ball <thomasjball@users.noreply.github.com>
2025-05-22 08:21:58 +0000

    add private palette

--- src/servers/bitmapserver.ts
+++ src/servers/bitmapserver.ts
@@ -6,6 +6,8 @@ import { JDServiceServer, JDServerOptions } from "../jdom/servers/serviceserver"
 export interface BitmapServerOptions extends JDServerOptions {
     width?: number
     height?: number
+    // 24bit rgb colors
+    palette?: number[]
 }
 
 /**
@@ -16,17 +18,33 @@ export class BitmapServer extends JDServiceServer {
     readonly width: JDRegisterServer<[number]>
     readonly height: JDRegisterServer<[number]>
 
+    private _palette: Uint8Array
     private _pixels: ImageData
 
     constructor(options?: BitmapServerOptions) {
         super(SRV_BITMAP, options)
 
-        const { width = 160, height = 120 } = options || {}
+        const { width = 160, 
+                height = 120, 
+                palette = [0xff0000, 0xffffff],
+            } = options || {}
 
         this.width = this.addRegister(BitmapReg.Width, [width])
         this.height = this.addRegister(BitmapReg.Height, [height])
+        
+        const pbuf = new Uint8Array(palette.length << 2)
+        for (let i = 0; i < palette.length; ++i) {
+            pbuf[i * 4] = (palette[i] >> 16) & 0xff
+            pbuf[i * 4 + 1] = (palette[i] >> 8) & 0xff
+            pbuf[i * 4 + 2] = palette[i] & 0xff
+            pbuf[i * 4 + 3] = 0xff
+        }
+        this._palette = pbuf
+
         this.addCommand(BitmapCmd.Fill, this.handleFill.bind(this))
     }
 
-    handleFill(pkt: Packet) {}
+    handleFill(pkt: Packet) {
+        // get the color index
+    }
 }
